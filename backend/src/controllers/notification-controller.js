174a144b6ce98b21c5ddfe240fef588eb6ff4b43
unknown <lucas.henrique.andrade.ti@gmai.com>
2025-04-30 20:50:34 -0300

    feat: notification-controller feito

---
+++ backend/src/controllers/notification-controller.js
@@ -0,0 +1,65 @@
+import {
+    createNewNotification,
+    getAllUserNotifications,
+    readNotification,
+    deleteNotification
+} from '../services/notification-service.js';
+
+// Cria uma notificação 
+const createNotification = async (req, res) => {
+    const { senderId, recipientId, formId, message } = req.body;
+
+    try {
+        const notification = await createNewNotification(senderId, recipientId, formId, message);
+        res.status(201).json({sucess: true, notification});
+    } catch (error) {
+        console.error('Erro ao criar notificação', error);
+        res.status(500).json({success: false, message: 'Erro ao criar notificação'});
+    }
+};
+
+// Obtem todas as notificações do usuário
+const getuserNotifications = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const notifications = await getAllUserNotifications(userId);
+        res.status(200).json({success: true, notifications});
+    } catch (error) {
+        console.error('Erro ao buscar notificações', error);
+        res.status(500).json({success: false, message: 'Erro ao buscar notificações'});
+    }
+};
+
+// Marca uma notificação como lida
+const markNotificationAsRead = async (req, res) => {
+    const { notificationId } = req.params;
+
+    try {
+        const notification = await readNotification(notificationId);
+        res.status(200).json({success: true, notification});
+    } catch (error) {
+        console.error('Erro ao marcar notificação como lida', error);
+        res.status(500).json({success: false, message: 'Erro ao marcar notificação como lida'});
+    }
+};
+
+// Deleta uma notificação
+const deleteUserNotification = async (req, res) => {
+    const { notificationId } = req.params;
+
+    try {
+        const notification = await deleteNotification(notificationId);
+        res.status(200).json({success: true, notification});
+    } catch (error) {
+        console.error('Erro ao deletar notificação', error);
+        res.status(500).json({success: false, message: 'Erro ao deletar notificação'});
+    }
+};
+
+export {
+    createNotification,
+    getuserNotifications,
+    markNotificationAsRead,
+    deleteUserNotification
+};
